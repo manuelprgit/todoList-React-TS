e66ab2dd6742b793c92e5439aa6e8db8be1884fb
manuelprgit <manuel.p.r@hotmail.com>
2024-06-19 07:23:34 -0400

    balblabla

--- src/components/TodoItems/TodoItems.tsx
+++ src/components/TodoItems/TodoItems.tsx
@@ -1,5 +1,4 @@
 import './TodoItems.scss'
-import { FaCheck, FaCircleXmark } from "react-icons/fa6";
 import { CheckIcon } from './icons/CheckIcon';
 import { DeleteIcon } from './icons/DeleteIcon';
 
