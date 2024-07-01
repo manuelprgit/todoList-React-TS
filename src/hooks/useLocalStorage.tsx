3903a179e2ba283f75e33838e4bd3b4d32a5251b
manuelprgit <manuel.p.r@hotmail.com>
2024-07-01 05:47:44 -0400

    Ahora si sincronice para enviar los cambios

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -4,32 +4,41 @@ type LocalStorageType = {
     items: Todo[],
     saveItems: (newItems: Todo[]) => void,
     loading: boolean,
-    error: string
+    error: boolean
 }
 
 
-const useLocalStorage = (itemName: string, initialValue: string): LocalStorageType => {
+const useLocalStorage = (itemName: string, initialValue: Todo[]): LocalStorageType => {
 
     const [items, setItems] = useState(initialValue); //Creamos un estado para guardar y actualizar la data al momento de asignarle un valor a setItems;
-
-    let parsedItems: any;
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
-
-        parsedItems = JSON.parse(localStorage.getItem(itemName) || initialValue); //Parceamos la información que vino del localStorage
-
-        const saveItems = (newItems: any) => {
-            localStorage.setItem(itemName, JSON.stringify(newItems));
-            setItems(newItems);
-        } //Esta función se encarga de guardar en el localStorage y actualizar el estado;
+        try {
+            const localStorageItem = localStorage.getItem(itemName);
+            let parsedItems: any;
+            (!localStorageItem) 
+                ? localStorage.setItem(itemName, JSON.parse(initialValue.toString()))
+                : parsedItems = JSON.parse(localStorageItem); //Parceamos la información que vino del localStorage
+            setLoading(false);
+            
+        } catch (error) {
+            setError(false);
+        }        
         
     }, []);
 
-    return { 
-        items, 
+    const saveItems = (newItems: any) => {
+        localStorage.setItem(itemName, JSON.stringify(newItems));
+        setItems(newItems);
+    } //Esta función se encarga de guardar en el localStorage y actualizar el estado;
+
+    return {
+        items,
         saveItems,
-        loading: false,
-        error: ''
+        loading,
+        error
     }; //retorna el nuevo estado y la función que se comunica con el actualizador del estado;
 
 } //custom hook
